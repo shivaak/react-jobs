39e25c09ac60b6faae15ce56ee02af94d0db3e07
Shivaa Krishnan <shivaak@uber.com>
2024-05-17 17:16:18 +0530

    Added react icons

--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaMapMarker } from "react-icons/fa";
 
 interface Company {
   name: string;
@@ -22,6 +23,13 @@ interface Props {
 }
 
 const JobListing = ({ job }: Props) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = job.description;
+  if (!showFullDescription) {
+    description = job.description.substring(0, 90) + "...";
+  }
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -30,7 +38,13 @@ const JobListing = ({ job }: Props) => {
           <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
 
-        <div className="mb-5">{job.description}</div>
+        <div className="mb-5">{description}</div>
+        <button
+          onClick={() => setShowFullDescription((prevState) => !prevState)}
+          className="text-indigo-500 mb-5 hover:text-indigo-600"
+        >
+          {showFullDescription ? "Less" : "More"}
+        </button>
 
         <h3 className="text-indigo-500 mb-2">{job.salary}</h3>
 
@@ -38,7 +52,7 @@ const JobListing = ({ job }: Props) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <i className="fa-solid fa-location-dot text-lg"></i>
+            <FaMapMarker className="inline text-lg mb-1 mr-1" />
             {job.location}
           </div>
           <a
