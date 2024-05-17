1996e230446e5df9a9dd2bbb4034d28a290559c1
Shivaa Krishnan <shivaak@uber.com>
2024-05-17 20:18:18 +0530

    Finish jobs page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,7 +11,7 @@ const HomePage = () => {
         subtitle="Find the React job that fits your skills and needs"
       />
       <HomeCards />
-      <JobListings />
+      <JobListings isHome={true} />
       <ViewAllJobs />
     </>
   );
